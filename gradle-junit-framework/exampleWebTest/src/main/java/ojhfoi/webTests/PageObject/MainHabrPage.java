7fc3723bf3fe1a36a83e2cdc075e3bbdec56bc03
ojhfoi <gabov.roman@gmail.com>
2023-10-13 16:18:12 +0500

    TEMP 2

--- gradle-junit-framework/exampleWebTest/src/main/java/ojhfoi/webTests/PageObject/MainHabrPage.java
+++ gradle-junit-framework/exampleWebTest/src/main/java/ojhfoi/webTests/PageObject/MainHabrPage.java
@@ -5,15 +5,14 @@ import ojhfoi.coreWeb.webHelpers.WebHelpers;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.security.Key;
 import java.time.Duration;
 import java.util.List;
 
@@ -24,6 +23,12 @@ public class MainHabrPage extends BasePage {
 
     @FindBy(xpath = "//article[@class='tm-articles-list__item']")
     private List<WebElement> articlesList;
+
+    @FindBy(xpath = "//a[contains(@href,'search')]")
+    private WebElement searchButton;
+
+    @FindBy(xpath = "//input[@name='q']")
+    private WebElement searchInput;
 
     @Step("Open block {blockName} in headers")
     public void openHeadersBlock (String blockName) {
@@ -53,11 +58,37 @@ public class MainHabrPage extends BasePage {
                                 .ignoring(ElementClickInterceptedException.class);
         wait.until(ExpectedConditions.visibilityOfAllElements(articlesList));
         Assertions.assertNotEquals(0, articlesList.size());
-        String articleTitle = articlesList.get(0)
+        String articleTitle = articlesList.get(articleCount)
                 .findElement(By.xpath("//a[@class='tm-title__link']//span"))
                 .getText();
         Assertions.assertNotNull(articleTitle);
         Assertions.assertNotEquals("", articleTitle);
         return articleTitle;
+    }
+
+    @Step("Check article title {articleTitle} is equals actual article title by num {articleCount}")
+    public void checkArticleTitle (int articleCount, String articleTitle) {
+        FluentWait<RemoteWebDriver> wait = new FluentWait<>(WebHelpers.init().getInstanceDriver())
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(ElementClickInterceptedException.class);
+        wait.until(ExpectedConditions.visibilityOfAllElements(articlesList));
+        Assertions.assertNotEquals(0, articlesList.size());
+        String actualArticleTitle = articlesList.get(articleCount)
+                .findElement(By.xpath("//a[@class='tm-title__link']//span"))
+                .getText();
+        Assertions.assertEquals(articleTitle, actualArticleTitle);
+    }
+
+    @Step("Open search page and search article {articleName}")
+    public void goToSearch (String articleName) {
+        searchButton.click();
+        FluentWait<RemoteWebDriver> wait = new FluentWait<>(WebHelpers.init().getInstanceDriver())
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(ElementClickInterceptedException.class);
+        wait.until(ExpectedConditions.visibilityOf(searchInput));
+        searchInput.sendKeys(articleName + Keys.ENTER);
+        wait.until(ExpectedConditions.visibilityOfAllElements(articlesList));
     }
 }
